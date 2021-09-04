b3ec4741fadd5af3c748e6b63eb64b77f58e3af8
Jailton Junior <jailton.junior94@outlook.com>
2021-09-03 22:07:42 -0300

    Criado o primeiro teste de unidade

---
+++ test/Finances.Tests/Application/Services/FinanceServiceTest.cs
@@ -0,0 +1,40 @@
+﻿using Moq;
+using Xunit;
+using Moq.AutoMock;
+using Finances.Tests.Mocks;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Finances.Business.Domain.Dtos;
+using Finances.Business.Domain.Entities;
+using Finances.Business.Domain.Interfaces;
+using Finances.Business.Application.Services;
+
+namespace Finances.Tests.Application.Services
+{
+    public class FinanceServiceTest
+    {
+        private readonly AutoMocker _mocker;
+        private readonly FinanceService _service;
+
+        public FinanceServiceTest()
+        {
+            _mocker = new AutoMocker();
+            _service = _mocker.CreateInstance<FinanceService>();
+        }
+
+        [Fact]
+        public async Task MustBeCreateFinance()
+        {
+            //Arrange
+            _mocker.GetMock<IFinanceRepository>()
+                .Setup(t => t.InsertAsync(It.IsAny<Finance>()))
+                .ReturnsAsync(FinanceMock.FinanceEntity());
+
+            //Act
+            var result = await _service.CreateFinanceAsync(It.IsAny<FinanceRequest>());
+
+            //Assert
+            Assert.Equal(StatusCodes.Status201Created, result.StatusCode);
+        }
+    }
+}
---
+++ test/Finances.Tests/Mocks/FinanceMock.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using Finances.Business.Domain.Entities;
+
+namespace Finances.Tests.Mocks
+{
+    public static class FinanceMock
+    {
+        public static Finance FinanceEntity()
+        {
+            var fake = new Faker<Finance>()
+                .CustomInstantiator(faker => new Finance(faker.Lorem.Paragraph(), faker.Random.Double(), "Entrada"))
+                .Generate();
+
+            return fake;
+        }
+    }
+}
